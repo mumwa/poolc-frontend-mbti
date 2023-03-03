88cf6525df77002cf0be1a768a4927538951d6f8
mumwa <mumwa@naver.com>
2023-03-03 19:45:51 +0900

    FIX : footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function Footer() {
   return (
@@ -9,6 +9,10 @@ function Footer() {
         poolc.official@gmail.com
         <br />
         회장 김의준 010-9916-2318
+        <br />
+        <br />
+        익명으로 응답을 통계 냅니다. (그냥 재미로 확인하려고요. 다른 용도로 쓰지
+        않습니다. AI가 학습 안함.)
       </div>
     </footer>
   );
